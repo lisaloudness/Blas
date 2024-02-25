5f6f640fb12b09a40ae44c907c7c8cc8bd2ec6ab
lisa jones <lisadjones75@gmail.com>
2024-02-25 10:30:53 +0000

    Set up Sign Up page

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -21,6 +22,13 @@ mongo = PyMongo(app)
 @app.route("/home")
 def home():
     return render_template("index.html")
+
+
+#--SIGN UP--
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
 
 
 @app.route("/")
