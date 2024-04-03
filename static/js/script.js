2cd412edfb35090644fe407ec28f8c8fdbc5eb7b
lisa jones <lisadjones75@gmail.com>
2024-04-03 20:46:19 +0000

    Added dynamic input ingredient fields to appear onclick

--- static/js/script.js
+++ static/js/script.js
@@ -11,11 +11,26 @@ $(document).ready(function(){
    
   });
 
+//Dynamic create ingredient field on add_recipe form
+$(document).ready(function () {
+  var ingredient_ = 0; // initialize to 0
 
+  var createInputs = function () {
+      ingredient_++; // Increment on each click
+      var inputHtml = '<div class="input-field col s6"><i class="material-icons prefix">local_dining</i><input type="text" id="ingredient_' + ingredient_ + '" name="ingredient_' + ingredient_ + '"><label for="ingredient_' + ingredient_ + '">Ingredient ' + ingredient_ + '</label></div>';
+      $("#ip_div").append(inputHtml);
+  };
 
-// Carousel 
-var instance = M.Carousel.init({
-  fullWidth: true
+  $("#btn").click(function () {
+      createInputs();
+      $("#form")
+    .find("input")
+    .each(function (i) {
+      $(this).attr("id", "ingredient_" + i);
+      $(this).attr("placeholder", "ingredient_" + i);
+      $(this).attr("name", "ingredient_" + i);
+    });
+  });
 });
-
+  
   
